4b1b3de406e8cca67f7c3612f7b9be088c943025
acer <acer@LAPTOP-TFSCGK6T>
2024-06-27 11:01:53 +0530

    This is my first core java project

---
+++ basicJava/src/basicJava/Sample2.java
@@ -0,0 +1,12 @@
+package basicJava;
+
+public class Sample2 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+        System.out.println("welcome to jfs");
+        System.out.print("jfs");
+	}
+
+}
